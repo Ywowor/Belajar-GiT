fce4ad121e4b87de9a4f24dbb0b6fe2862c27c50
Yosua Wowor <yosuammwowor@gmail.com>
2023-04-02 20:56:37 +0800

    fix:bug on scrolling forward and backward + add: some property in css file

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 let slideIndex = 1;
 showSlide(slideIndex);
 
+// Section button
 function plusSlide(n) {
     showSlide(slideIndex += n);
 }
 
+// Section dots
 function currentSlide(n) {
     showSlide(slideIndex = n);
 }
@@ -13,6 +15,10 @@ function showSlide(n) {
     let i;
     let slides = document.getElementsByClassName("mySlide");
     let dot = document.getElementsByClassName("dots");
+
+    // Section bug scrolling forward and backward
+    if (slideIndex > slides.length) {slideIndex = 1};
+    if (slideIndex < 1) {slideIndex = slides.length};
 
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -23,5 +29,4 @@ function showSlide(n) {
 
     slides[slideIndex-1].style.display = "block";
     dot[slideIndex-1].className += " active";
-
 }
